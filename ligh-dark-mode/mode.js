9fc66ca459cf9f72ecc1de6191f7ee1ea9b38e5e
chiaszu <chiaszu.21@gmail.com>
2021-12-09 22:16:53 +0800

    update

--- ligh-dark-mode/mode.js
+++ ligh-dark-mode/mode.js
@@ -1,16 +1,32 @@
-window.onload = theme_button;
-function change() {
-  var elem = document.getElementById("theme_button");
-  elem.innerText = elem.innerText === "Dark Mode" ? "Light Mode" : "Dark Mode";
-}
+// window.onload = theme_button;
+// function change() {
+//   var elem = document.getElementById("theme_button");
+//   elem.innerText = elem.innerText === "Dark Mode" ? "Light Mode" : "Dark Mode";
+// }
 
-function changeTheme() {
+// function changeTheme() {
+//   var elem = document.getElementsByTagName("body")[0];
+//   elem.className = elem.className === "" ? "dark" : "";
+//   change();
+// }
+
+// function theme_button() {
+//   var button = document.getElementById("theme_button");
+//   button.onclick = changeTheme;
+// }
+
+window.onload = theme_button;
+
+function theme_button() {
+  var button = document.getElementById("theme_button");
+  button.onclick = change_Theme;
+}
+function change_Theme() {
   var elem = document.getElementsByTagName("body")[0];
   elem.className = elem.className === "" ? "dark" : "";
   change();
 }
-
-function theme_button() {
-  var button = document.getElementById("theme_button");
-  button.onclick = changeTheme;
+function change() {
+  var elem = document.getElementById("theme_button");
+  elem.innerText = elem.innerText === "Dark Mode" ? "Light Mode" : "Dark Mode";
 }
